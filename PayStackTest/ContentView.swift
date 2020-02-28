616bec6c0fe06b9b6b0744bc7caa101f039edf8a
fatehkhann <fatehkhann@gmail.com>
2020-02-28 19:44:16 +0500

    Initial Commit

---
+++ PayStackTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PayStackTest
+//
+//  Created by Zarrar Company on 28/02/2020.
+//  Copyright © 2020 Zarrar Company. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
